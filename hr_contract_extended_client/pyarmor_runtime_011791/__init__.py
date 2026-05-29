93866c369cb14ba1c879870bd5119ce5b7b3d153
miguelpardo21 <66836444+miguelpardo21@users.noreply.github.com>
2026-05-29 14:42:47 -0500

    [ADD] Add manuals and final advance 2

--- hr_contract_extended_client/pyarmor_runtime_011791/__init__.py
+++ hr_contract_extended_client/pyarmor_runtime_011791/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:21:50.651024
+# Pyarmor 9.2.4 (pro), 011791, 2026-05-29T14:40:05.768273
 from odoo.addons.hr_contract_extended_client.pyarmor_runtime_011791.pyarmor_runtime import __pyarmor__
